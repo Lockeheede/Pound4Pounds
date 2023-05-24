46a2de251877d3eb0cb21b0b37c3142d847e01f9
Prescott Lawani <38223423+Lockeheede@users.noreply.github.com>
2023-05-24 15:35:55 -0500

    Laptop Update
    ->With Benny's help
    :Fixed the Pounding.cs to use a State Machine
    :Worked and learned the basics of the Debugger
    ->Needed
    :Scoring System When unPounding Cakes

--- Assets/Scripts/Pounding.cs
+++ Assets/Scripts/Pounding.cs
@@ -6,31 +6,77 @@ public class Pounding : MonoBehaviour
 {
     public float speed = 20f;
     private Rigidbody m_RigidBody;
+    public Vector3 startingPos = new Vector3(0,0,0);
     private bool pounded = false;
     private float poundReset = 1.5f;
     public GameObject poundEffect;
     public GameObject poundLocation;
+    public enum PoundingState
+    {
+        PoundDown,
+        PoundUp,
+        PoundNeutral
+    };
+    PoundingState poundingState = PoundingState.PoundNeutral;
+    public float stateTimer = 0.0f;
 
     // Start is called before the first frame update
     void Start()
     {
         m_RigidBody = GetComponent<Rigidbody>();
+        startingPos = m_RigidBody.transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.Space) || Input.GetKey(KeyCode.Mouse0) && pounded == false) 
-        { 
-            m_RigidBody.AddForce(Vector3.down * speed);
-            Invoke("PoundEffect", 0.5f);
+        switch (poundingState)
+        {
+            case PoundingState.PoundDown:
+                PoundDown();
+                break;
+            case PoundingState.PoundUp:
+                PoundUp();
+                break;
+            case PoundingState.PoundNeutral:
+                PoundNeutral();
+                break;
+            default:
+                Debug.Log("No State Found!");
+                break;
         }
+    }
+
+    void PoundNeutral()
+    {
+        m_RigidBody.velocity = Vector3.zero;
+        if (Input.GetKey(KeyCode.Space) || Input.GetKey(KeyCode.Mouse0))
+        {
+            poundingState = PoundingState.PoundDown;
+            stateTimer = Time.time;
+        }
+    }
+
+    void PoundUp()
+    {
+        m_RigidBody.AddForce(Vector3.up * speed);
+        if (m_RigidBody.transform.position.y >= startingPos.y)
+        {
+            m_RigidBody.transform.position = startingPos;
+            poundingState = PoundingState.PoundNeutral;
+        }
+    }
+
+    void PoundDown()
+    {
+        m_RigidBody.AddForce(Vector3.down * speed);
+        if (Time.time - stateTimer > 1f)
+            poundingState = PoundingState.PoundUp;
     }
 
     void PoundEffect()
     {
         Instantiate(poundEffect, poundLocation.transform);
-        m_RigidBody.AddForce(Vector3.up * speed);
         pounded = true;
         Invoke("StopLift", 0.3f);
     }
